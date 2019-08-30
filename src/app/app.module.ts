b382038c34f8f7b3177cedfc5ba8e8fa3287b036
subhrajyoti <subhrajyotipradhan@gmail.com>
2019-08-30 18:47:34 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MultipleMatTableTaskComponent } from './multiple-mat-table-task/multiple-mat-table-task.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultipleMatTableTaskComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+		MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
+    //MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
